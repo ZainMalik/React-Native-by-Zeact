56d76fa8a5335844ba69e4828c89912d6b518fba
ZainMalik <zainmalik1990@gmail.com>
2025-03-22 19:35:45 -0700

    Lesson 2 Completed

---
+++ app/(coffee)/_layout.jsx
@@ -0,0 +1,9 @@
+import { Slot } from "expo-router";
+
+
+export default function CoffeeLayout() {
+    return (
+        <Slot></Slot>
+)
+    
+}
---
+++ app/(tabs)/contact.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Timings = () => {
+  return (
+    <View>
+      <Text>Timings</Text>
+    </View>
+  )
+}
+
+export default Timings
